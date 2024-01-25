5faa5970936320558ddafb7d4f6e7a186c599769
Hoang1001 <duchoang.embedded@gmail.com>
2024-01-25 18:59:58 +0700

    End of Section 9-2

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,3 +1,101 @@
-export default function Login() {
-  return <h1>Login</h1>;
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import agent from "../../app/api/agent";
+import { FieldValues, useForm } from "react-hook-form";
+import { LoadingButton } from "@mui/lab";
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {"Copyright © "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function LogIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors, isValid },
+  } = useForm({ mode: "onTouched" });
+
+  async function submitForm(data: FieldValues) {
+    try {
+      await agent.Account.login(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              fullWidth
+              label="Username"
+              autoComplete="email"
+              autoFocus
+              {...register("username", { required: "Username is required" })}
+              error={!!errors.username}
+              helperText={errors.username?.message as string}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              label="Password"
+              type="password"
+              {...register("password", { required: "Password is required" })}
+              error={!!errors.password}
+              helperText={errors.password?.message as string}
+            />
+
+            <LoadingButton
+              disabled={!isValid}
+              loading={isSubmitting}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </LoadingButton>
+            <Grid container>
+              <Grid item>
+                <Link to="/register">{"Don't have an account? Sign Up"}</Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
